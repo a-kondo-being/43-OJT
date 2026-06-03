e6e4711a8c32b20299d70d47cd6d3783e30e787e
k-mori5 <k-mori@beingcorp.co.jp>
2026-06-03 13:48:43 +0900

    #624689 プレイヤークラスの実装
    プレイヤークラスを実装しました。

---
+++ AcchimuitehoiQuest/AcchimuitehoiQuest/player.cs
@@ -0,0 +1,37 @@
+﻿namespace AcchimuitehoiQuest
+{
+    public class player
+    {
+        public int HP { get; set; }
+        public int JankenHand { get; set; }
+        public int Direction { get; set; }
+
+        public player()
+        {
+            HP = 3;
+            JankenHand = 0;
+            Direction = 0;
+        }
+        public void TakeDamage(int damage)
+        {
+            HP -= damage;
+            if (HP < 0)
+            {
+                HP = 0;
+            }
+        }
+
+        public bool IsAlive()
+        {
+            if (HP > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
