797a5e6abcb0b5ff6eba0f220ed5a8b89fd0b937
k-mori5 <k-mori@beingcorp.co.jp>
2026-06-03 16:29:34 +0900

    #624690　あっち向いてホイ機能の実装
    上記機能の詳細は以下の通りです。
    ・向きやすい方向に40％の傾斜
    ・プレイヤーと敵の向く方向の一致判定メソッド

---
+++ AcchimuitehoiQuest/AcchimuitehoiQuest/Acchimuitehoi.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace AcchimuitehoiQuest
+{
+    public enum Direction
+    {
+        Up = 0,
+        Down = 1,
+        Left = 2,
+        Right = 3,
+    }
+    public class AcchimuitehoiLogic
+    {
+        private Random random = new Random();
+
+        public Direction DetermineEnemyDirection(Direction easyDirection)
+        {
+            int randomValue = random.Next(1, 101);
+
+            if (randomValue <= 40)
+            {
+                return easyDirection;
+            }
+            Direction result;
+            do
+            {
+                int randomNumber = random.Next(0, 4);
+                result = (Direction)randomNumber;
+            }
+            while (result == easyDirection);
+
+            return result;
+        }
+        public bool IsSuccess(Direction playerDirection, Direction enemyDirection)
+        {
+            return playerDirection == enemyDirection;
+        }
+    }
+}
