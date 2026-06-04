5d441f8995b45671a9ff7479712e03d35071de62
a-kondo1 <a-kondo@beingcorp.co.jp>
2026-06-04 14:20:16 +0900

    #624692 バトル進行管理クラスの実装
    じゃんけんの勝敗で分かれる処理を作成しました。
    あっち向いてホイの勝敗で分かれる処理を作成しました。

---
+++ AcchimuitehoiQuest/AcchimuitehoiQuest/BattleManager.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsFormsApp1;
+
+namespace AcchimuitehoiQuest
+{
+    public class BattleManager
+    {
+        public int PlayerHP { get; set; }          // プロパティの定義
+        public Enemy CurrentEnemy { get; set; }        // 現在戦っている敵のデータを入れておくプロパティ
+        public string CurrentPhase { get; set; }
+
+        public BattleManager()
+        {
+            this.PlayerHP = 3;                      // プレイヤーのHPは初期値3
+            this.CurrentPhase = "じゃんけん";
+        }
+        public void SetupEnemy(Enemy enemy)        // 戦いを始めるときの処理（台本）
+        {
+            this.CurrentEnemy = enemy;              // 引数で渡された敵のデータをプロパティに入れる
+        }
+
+
+        public void OnJankenLose()
+        {
+            this.PlayerHP--;                      // じゃんけんに負けたときはプレイヤーのHPを1減らす
+          
+        }
+
+        public void OnJankenwin()
+        {
+            this.CurrentPhase = "あっち向いてホイ"; // じゃんけんに勝ったときはフェーズを「あっち向いてホイ」にする
+        }
+
+        public void OnAcchimuiteHoiResult(bool isSuccess)
+        {
+            // 【ガード節1】失敗した場合は、じゃんけんに戻してすぐ処理を終了する
+            if (!isSuccess)
+            {
+                this.CurrentPhase = "じゃんけん";
+                return;
+            }
+
+            // 【ガード節2】敵がセットされていない異常事態なら、エラーを防ぐため処理を抜ける
+            if (this.CurrentEnemy == null)                               
+            {
+                return;
+            }
+
+            // --- ここから下は「成功した」かつ「敵が存在する」ことが保証された処理 ---
+
+            
+            this.CurrentEnemy.HP--;         // 敵のHPを1減らす
+           
+            // もし敵のHPがまだ残っていれば、じゃんけんフェーズに戻す
+            if (this.CurrentPhase != "ステージクリア")
+            {
+                this.CurrentPhase = "じゃんけん";
+            }
+        }
+
+      
+    }
+}
+
