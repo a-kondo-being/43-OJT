1ef0d166bc53d914ab53fa593d68afbdb1a86b2d
s-sato2 <s-sato@beingcorp.co.jp>
2026-06-04 14:36:06 +0900

    ・タイトル名を表示する ・「冒険を始める」ボタンを配置する

---
+++ AcchimuitehoiQuest/AcchimuitehoiQuest/TitleForm.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace AcchimuitehoiQuest
+{
+    partial class TitleForm
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TitleForm));
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button2.Font = new System.Drawing.Font("Pristina", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(550, 619);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(215, 94);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Start";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // TitleForm
+            // 
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1305, 773);
+            this.Controls.Add(this.button2);
+            this.DoubleBuffered = true;
+            this.Name = "TitleForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ AcchimuitehoiQuest/AcchimuitehoiQuest/TitleForm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace AcchimuitehoiQuest
+{
+    public partial class TitleForm : Form
+    {
+        public TitleForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //QusetForm questform = new QuestForm();
+
+            //questform.Show();
+
+            this.Hide();
+
+        }
+    }
+}
