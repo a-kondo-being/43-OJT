95a37544eb4a8ffde751d5f33338abbcf91e3fe3
k-mori5 <k-mori@beingcorp.co.jp>
2026-06-04 11:40:11 +0900

    #624690　あっち向いてホイ機能の実装
    不要なコードを削除しました

--- AcchimuitehoiQuest/AcchimuitehoiQuest/Acchimuitehoi.cs
+++ AcchimuitehoiQuest/AcchimuitehoiQuest/Acchimuitehoi.cs
@@ -3,14 +3,6 @@ using System.Collections.Generic;
 
 namespace AcchimuitehoiQuest
 {
-    public enum Direction
-    {
-        Up = 0,
-        Down = 1,
-        Left = 2,
-        Right = 3,
-    }
-
     public class AcchimuitehoiLogic
     {
         public enum Direction
